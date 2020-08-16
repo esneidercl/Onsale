3fcf0bced40159d103e1a5fd7b04a47b6c3e1d50
ESNEIDER CANO LONDOÑO <esneidercano205587@correo.itm.edu.co>
2020-08-16 14:00:44 -0500

    implementacion de CRUD en countries

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+   public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50,ErrorMessage ="the file {0} must contain less than {1} characteres")]// asignar el numero de caracteres en la bd,error mesas para imprimir un mensaje al usuario
+        [Required]// para hacer que un campo sea requerido, se requiere importar paquete con alt+ enter
+        public string Name{ get; set; }
+
+    }
+}
---
+++ OnSale.Web/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnSale.Common.Entities;
+
+namespace OnSale.Web.Data
+{
+    public class DataContext : DbContext // para heredar, requiere importar 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)// metodo constructor ctro 2 veces tab, con parametro
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)// para validar que no se repitan 2 paises
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()// referencia de la entidad a evaluar
+                .HasIndex(t => t.Name) // crear un indice a la entidad por el campo nombre, la t es un objeto
+                .IsUnique();// instruccion para hacer unico un campo
+        }
+    }
+}
